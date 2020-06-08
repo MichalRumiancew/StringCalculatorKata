24f1c39cb344832f03b5c9d4a6d531e322af646b
Michal <michalr11@gmail.com>
2020-06-08 16:16:23 +0200

    First step,

---
+++ src/test/java/CalculatorTest1.java
@@ -0,0 +1,45 @@
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//public class CalculatorTest1
+//
+//
+//    private Calculator calculator;
+//
+//    @BeforeEach
+//    void initCalculator() {
+//        calculator = new Calculator();
+//    }
+//
+//    @Test
+//    public void shouldReturnZeroWhenEmptyStringGiven() {
+//        int result = calculator.add("");
+//        assertThat(result).isEqualTo(0);
+//    }
+//
+//    @Test
+//    void shouldReturnOneWhenOneGiven() {
+//        int result = calculator.add("1");
+//        assertThat(result).isEqualTo(1);
+//    }
+//
+//    @Test
+//    void shouldReturnTwoWhenTwoGiven() {
+//        int result = calculator.add("2");
+//        assertThat(result).isEqualTo(2);
+//    }
+//
+//    @Test
+//    void shouldReturnSumOfTwoNumbers() {
+//        int result = calculator.add("1,2");
+//        assertThat(result).isEqualTo(3);
+//    }
+//
+//    @Test
+//    void shouldReturnSumOfThreeNumbers() {
+//        int result = calculator.add("1,2,3");
+//        assertThat(result).isEqualTo(6);
+//    }
+//}
